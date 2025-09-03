9e54b9999a28f7b7184c990b5c7d2501f5e461de
DMandar8 <mandardeshmukh863@gmail.com>
2025-09-03 20:14:41 +0530

    New Changes

--- DevOps.java
+++ DevOps.java
@@ -1,5 +1,5 @@
 public class DevOps {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins Pipeline!");
+        System.out.println("Hello, Everyone!");
     }
 }
